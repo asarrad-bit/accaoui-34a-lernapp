16564d53f5eb0e0cbc15bed61ac61655b86d44ec
Ali Sarrad <a.sarrad@gmail.com>
2026-05-28 20:28:05 +0200

    v23.3.0 prepare oral exam module files

---
+++ oral-sheets.js
@@ -0,0 +1,13 @@
+/* =====================================================
+   ACCAOUI §34a LERN-APP
+   oral-sheets.js
+   Zweck:
+   - spätere Prüfungsbögen für die mündliche Prüfung
+   - aktuell nur vorbereitet
+===================================================== */
+
+console.log("Accaoui oral-sheets.js geladen");
+
+if (!window.ACCAOUI_ORAL_SHEETS_MODULE) {
+  window.ACCAOUI_ORAL_SHEETS_MODULE = true;
+}
